b5deeaa33c8faf9eb279259fb2873142f3dfebc6
exampleOne <a.vasilievaaa@yandex.ru>
2022-03-24 21:20:55 +0300

    Домашнее задание №13

---
+++ src/ru/skypro/Bicycle.java
@@ -0,0 +1,8 @@
+package ru.skypro;
+
+public class Bicycle extends Transport {
+public Bicycle (String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+public void updateTyre () {System.out.println("Меняем покрышку");}
+}
